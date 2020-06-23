5aced75196103219dcb10a5e8ddc7a3a66de97da
Farnan130117 <mh.farnan@gmail.com>
2020-06-23 23:50:41 +0600

    adding project class as a model in (base -> model.py) file

--- base/models.py
+++ base/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class project(models.Model):
+	name = models.CharField(max_length=200, null=True)
+	gitrepo = models.CharField(max_length=200, null=True)
+	date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+	def __str__(self):
+		return self.name
