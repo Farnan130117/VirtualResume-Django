47464427ae0f7edfcbb676541ad8c3f6d22c47ca
Farnan130117 <mh.farnan@gmail.com>
2020-06-25 23:45:26 +0600

    Project model add, configure and view

--- base/views.py
+++ base/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def home(request):
 	return render(request, 'base/home.html')
+
+def projects(request):
+	projects = project.objects.all()
+	return render(request, 'base/projects.html', {'projects':projects})
