0d239e78213fec9c876a3803e46dfb9c2efbf345
Farnan130117 <mh.farnan@gmail.com>
2020-07-03 23:53:08 +0600

    add image field in project model as picture varible and its configuration

--- base/models.py
+++ base/models.py
@@ -7,6 +7,10 @@ class project(models.Model):     #project_class
 
 	name = models.CharField(max_length=200, null=True)
 	gitrepo = models.CharField(max_length=200, null=True)
+	picture = models.ImageField(null=True)    
+	  #pip install pillow
+	  #Pillow is a Python Imaging Library which deals with different image files. 
+	  #We won’t be using pillow directly but Django is using it. Therefore, we need pillow library.
 	date_created = models.DateTimeField(auto_now_add=True, null=True)
 
 	def __str__(self):
