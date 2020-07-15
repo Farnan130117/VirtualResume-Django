f7599d03dc37ecedd124fc0506b5f0735350dc95
Farnan130117 <mh.farnan@gmail.com>
2020-07-15 22:55:24 +0600

    Adding comment in upadte & delete project related sections

--- base/urls.py
+++ base/urls.py
@@ -6,10 +6,10 @@ from . import views
 urlpatterns	=	[
 
 	path('',views.home,name='home'),
-	path('projects/',views.projects,name='projects'),
+	path('projects/',views.projects,name='projects'),  #viewing all projects
 	path('projects/upload/',views.upload,name='upload'),  # we can upload new project in upload section
-	path('projects/update/<int:project_id>', views.update_project,name='update'),
-	path('projects/delete/<int:project_id>', views.delete_project,name='delete'),
+	path('projects/update/<int:project_id>', views.update_project,name='update'), # Edit project
+	path('projects/delete/<int:project_id>', views.delete_project,name='delete'), # delete project
 	path('vlog/',views.vlog), # used for vedio vlog 
 	
 ]
